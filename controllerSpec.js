18233021cbd9ed0a7fec6934cbaef1a2c249a646
Jim Compher <jimmy.compher@gmail.com>
2017-09-05 16:43:10 -0400

    Add angular files for testing, create simple unit tests.

---
+++ controllerSpec.js
@@ -0,0 +1,34 @@
+describe('Controller : ListCtrl', function() {
+
+  // new version of the module
+  beforeEach(module('notesApp'));
+
+  var ctrl;
+
+  // instantiate new version of the controller
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ListCtrl');
+  }));
+
+  it ('should have items available on load', function() {
+    expect(ctrl.items).toEqual([
+      {id: 1, label: 'First', done: true},
+      {id: 2, label: 'Second', done: false}
+    ]);
+  });
+
+  it('should have highlight items based on state', function() {
+    var item = {id: 1, label: 'First', done: true};
+
+    var actualClass = ctrl.getDoneClass(item);
+    expect(actualClass.finished).toBeTruthy();
+    expect(actualClass.unfinished).toBeFalsy();
+    
+    item.done = false;
+    actualClass = ctrl.getDoneClass(item);
+    expect(actualClass.finished).toBeFalsy();
+    expect(actualClass.unfinished).toBeTruthy();
+    
+  });
+
+});
