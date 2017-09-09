3e5d1da1fdfd8d597d14ab446bf1d90257b66092
Jim Compher <jimmy.compher@gmail.com>
2017-09-09 13:14:23 -0400

    Create simple service using a factory function

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 angular.module('notesApp', [])
   .controller('MainCtrl', [function () {
     var self = this;
-
     self.tab = 'first';
-    self.open = function(tab) {
+    self.open = function (tab) {
       self.tab = tab;
     };
   }])
-  .controller('SubCtrl', [function() {
+  .controller('SubCtrl', ['ItemService', function (ItemService) {
     var self = this;
-  
-    console.log('inside subctrl');
-    
-    self.list = [
-      {id: 1, label: 'Item 0'},
-      {id: 2, label: 'Item 1'}
-    ];
-  
-    self.add = function() {
-      self.list.push({
-        id: self.list.length + 1,
-        label: 'Item ' + self.list.length
+    self.list = function () {
+      return ItemService.list();
+    };
+    self.add = function () {
+      ItemService.add({
+        id: self.list().length + 1,
+        label: 'Item ' + self.list().length
       });
-    };  
-  
+    };
+  }])
+  .factory('ItemService', [function () {
+    var items = [{
+        id: 1,
+        label: 'Item 0'
+      },
+      {
+        id: 2,
+        label: 'Item 1'
+      }
+    ];
+
+    return {
+      list: function () {
+        return items;
+      },
+      add: function (item) {
+        items.push(item);
+      }
+    };
   }]);
