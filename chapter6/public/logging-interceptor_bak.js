a3abc7b3a0547a033a618c68322977bc5a1bfcdd
Jim Compher <jimmy.compher@gmail.com>
2017-09-13 15:02:40 -0400

    Add logging interceptor to test error messages.

--- chapter6/public/logging-interceptor_bak.js
+++ chapter6/public/logging-interceptor_bak.js
@@ -1,3 +1,4 @@
+// File: chapter6/public/logging-interceptor.js
 angular.module('notesApp', [])
   .controller('MainCtrl', ['$http', function($http) {
     var self = this;
@@ -7,7 +8,7 @@ angular.module('notesApp', [])
       return $http.get('/api/note').then(function(response) {
         self.items = response.data;
       }, function(errResponse) {
-        console.log('Error while fetching notes');
+        console.error('Error while fetching notes');
       });
     };
 
@@ -20,38 +21,44 @@ angular.module('notesApp', [])
           self.newTodo = {};
         });
     };
-  }])
-  .factory('MyLoggingInterceptor', ['$q', function($q) {
+
+  }]).factory('MyLoggingInterceptor', ['$q', function($q) {
     return {
       request: function(config) {
         console.log('Request made with ', config);
-        //error/not allowed
-        return $q.reject('Not allowed');
-        //return config;
+        return config;
+        // If an error, or not allowed, or my custom condition
+        // return $q.reject('Not allowed');
       },
       requestError: function(rejection) {
         console.log('Request error due to ', rejection);
+        // Continue to ensure that the next promise chain
+        // sees an error
         return $q.reject(rejection);
-        //ok/no error - return someValue;
+        // Or handled successfully?
+        // return someValue;
       },
       response: function(response) {
-        console.log('Response from server ', response);
-        // return a promise
+        console.log('Response from server', response);
+        // Return a promise
         return response || $q.when(response);
       },
       responseError: function(rejection) {
         console.log('Error in response ', rejection);
-        // can check auth status code
-        if (rejection.status === 403) {
-          // show login
-          // return value to tell controllers this has been handled
-        }
-        // or return rejection to continue promise failure chain
+        // Continue to ensure that the next promise chain
+        // sees an error
+        // Can check auth status code here if need to
+        // if (rejection.status === 403) {
+        //   Show a login dialog
+        //   return a value to tell controllers it has
+        // been handled
+        // }
+        // Or return a rejection to continue the
+        // promise failure chain
         return $q.reject(rejection);
       }
     };
   }])
   .config(['$httpProvider', function($httpProvider) {
-    console.log('Current number of interceptors: ', $httpProvider.interceptors.length);
     $httpProvider.interceptors.push('MyLoggingInterceptor');
   }]);
