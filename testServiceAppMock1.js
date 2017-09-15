a0a30938018e562f23a79bbda92e5ea879f48e31
Jim Compher <jimmy.compher@gmail.com>
2017-09-15 12:12:53 -0400

    Modify itemservice mock file

--- testServiceAppMock1.js
+++ testServiceAppMock1.js
@@ -2,7 +2,7 @@ angular.module('notesApp1Mocks', [])
   .factory('ItemService', [function() {
     return {
       list: function() {
-        return [{id: 1, label: 'Moc'}];
+        return [{id: 1, label: 'Mock'}];
         }
       };
   }]);
