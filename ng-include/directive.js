da67d6c14147775121be1bc9758287a1bf5423c6
Jim Compher <jimmy.compher@gmail.com>
2017-09-20 17:07:14 -0400

    Add buttons to demonstrate different scopes within a ng-repeat and directive

--- ng-include/directive.js
+++ ng-include/directive.js
@@ -9,7 +9,14 @@ angular.module('stockMarketApp')
       link: function($scope, $element, $attrs) {
         $scope.getChange = function(stock) {
           return Math.ceil(((stock.price - stock.previous) / stock.previous) * 100);          
-        }
+        };
+        $scope.changeStock = function() {
+          $scope.stockData = {
+            name: 'Directive Stock',
+            price: 500,
+            previous: 100
+          };
+        };
       }
     };
   }]);
