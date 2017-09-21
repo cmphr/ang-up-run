42f52bfd951924967b3a4845f22fd4bcd59dbb89
Jim Compher <jimmy.compher@gmail.com>
2017-09-21 17:14:33 -0400

    Modify custom directive to use different scope types

--- ng-include/app.js
+++ ng-include/app.js
@@ -10,18 +10,21 @@ angular.module('stockMarketApp', [])
         name: 'Second Stock',
         price: 140,
         previous: 120
+      },
+      {
+        name: 'Third Stock',
+        price: 110,
+        previous: 110
+      },
+      {
+        name: 'Fourth Stock',
+        price: 400,
+        previous: 420
       }
     ];
-    self.changeAllStocks = function () {
-      for (var i = 0; i < 4; i++) {
-        self.stocks[i] = {
-          name: 'Controller Stock',
-          price: 200,
-          previous: 250
-        }
-      }
+
+    self.onStockSelect = function(price, name) {
+      console.log('Selected Price ', price, 'Name ', name);
     };
-    self.changeFirstStock = function() {
-      self.stocks[0].name = 'Changed First Stock';
-    }
+
   }]);
--- ng-include/directive.js
+++ ng-include/directive.js
@@ -4,18 +4,21 @@ angular.module('stockMarketApp')
       templateUrl: 'stock.html',
       restrict: 'A',
       scope: {
-        stockData: '='
+        stockData: '=',
+        stockTitle: '@',
+        whenSelect: '&'
       },
       link: function($scope, $element, $attrs) {
         $scope.getChange = function(stock) {
           return Math.ceil(((stock.price - stock.previous) / stock.previous) * 100);          
         };
-        $scope.changeStock = function() {
-          $scope.stockData = {
-            name: 'Directive Stock',
-            price: 500,
-            previous: 100
-          };
+
+        $scope.onSelect = function() {
+          $scope.whenSelect({
+            stockName: $scope.stockData.name,
+            stockPrice: $scope.stockData.price,
+            stockPrevious: $scope.stockData.previous
+          });
         };
       }
     };
