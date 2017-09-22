8e00c5434cd2ff8d724711c32656f4fa753be929
Jim Compher <jimmy.compher@gmail.com>
2017-09-22 17:10:12 -0400

    Add comments to help describe transclude workings

--- transclusions/directive.js
+++ transclusions/directive.js
@@ -9,13 +9,19 @@ angular.module('stockMarketApp')
         var container = angular.element('<div class="container"></div>');
 
         for (var i = 0; i < myArray.length; i++) {
+          // $transclude is a constructor that allows
+          // create an element instance with a new child
+          // scope using the clone linking function
           var instance = $transclude($scope.$new(), function(clonedElement, newScope) {
+            // expose custom variables for the instance
             newScope.currentIndex = i;
             newScope.stock = myArray[i];
           });
           container.append(instance);
         }
 
+        // original element is replaced with a comment
+        // so add the new container after it
         $element.after(container);
       }
     };
