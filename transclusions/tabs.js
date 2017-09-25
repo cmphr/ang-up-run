44e567033a84cbb3a32057690c7835e46d03728f
Jim Compher <jimmy.compher@gmail.com>
2017-09-25 12:58:47 -0400

    Add controller key to the directive

--- transclusions/tabs.js
+++ transclusions/tabs.js
@@ -11,6 +11,28 @@ angular.module('stockMarketApp')
             '<span ng-bind="tab.title"></span>' +
         '</div>' +
       '</div>' +
-      '<div ng-transclude></div>'
+      '<div ng-transclude></div>',
+      controller: function($scope) {
+        var currentIndex = 0;
+        $scope.tabs = [];
+        this.registerTab = function(title, scope) {
+          if ($scope.tabs.length === 0) {
+            scope.selected = true;
+          }
+          else {
+            scope.selected = false;
+          }
+          $scope.tabs.push({title: title, scope: scope});
+        };
+        $scope.selectTab = function(index) {
+          currentIndex = index;
+          for (var i = 0; i < $scope.tabs.length; i++) {
+            $scope.tabs[i].scope.selected = currentIndex === i;
+          }
+        };
+        $scope.isSelectedTab = function(index) {
+          return currentIndex === index;
+        };
+      }
     };
   }]);
