3ed3ae8cf376bfd77b0fc5fec2ef55234c1ddeaf
Jim Compher <jimmy.compher@gmail.com>
2017-09-28 16:57:13 -0400

    Add directive wrapper for slider component

---
+++ directive-slider/noui-slider.js
@@ -0,0 +1,32 @@
+angular.module('sliderApp')
+  .directive('noUiSlider', [function() {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      link: function($scope, $element, $attr, ngModelCtrl) {
+        $element.noUiSlider({
+          start: 0, // might not have the initial value in ngModelCtrl yet
+          range: {
+            // $attrs gives us string values by default
+            // noUiSlider expects numbers so we need to convert
+            min: Number($attr.rangeMin),
+            max: Number($attr.rangeMax)
+          }
+        });
+
+        // when data in angular changes, notify third party directive
+        ngModelCtrl.$render = function() {
+          $element.val(ngModelCtrl.$viewValue);
+        };
+
+        // when data changes outside of angular
+        $element.on('set', function(args) {
+          // tell angular that it needs to update the UI
+          $scope.$apply(function() {
+            // set the data within angular
+            ngModelCtrl.$setViewValue($element.val());
+          });
+        });
+      }
+    };
+  }]);
